b78737482a4e8d8e92b26f37b3140abaade7498a
Andrew Shen <39780709+AndrewShen31@users.noreply.github.com>
2018-07-23 09:34:02 +0800

    Added more constans and options

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+# !=====! IMMUTABLE CONSTANTS !=====!
+
 # ( width or x, height or y)
 SCREEN_SIZE = (1960, 1080)
 PIXEL_SZ = 5  # the most base unit of the game.
@@ -11,10 +13,15 @@ VIEWBOX_SMOOTHER = 5  # pixels
 
 # collision
 COLLISION_SZ = 2  # DEFUALT IS 2; BUT do to the fall speed I must do make the sz 2 else it would go through blocks
-TOP_PAD = 5
-BOT_PAD = -5
+TOP_PAD = 4
+BOT_PAD = -4
 RGT_PAD = 5
 LFT_PAD = -11
+
+# placing and breaking
+RAYCAST_LINE_DEG = 50  # Degree; number
+RAYCAST_MAX_CIRCLE = 8  # Radius; block
+RAYCAST_PP_SIZE = 5
 
 # jumping mechanism
 FALL_SPEED = 5
@@ -28,6 +35,9 @@ EAST  = 1  # Right
 SOUTH = 2  # Bottom
 WEST  = 3  # Left
 NORTH = 4  # Top
+
+RIGHT_CLK = 1
+LEFT_CLK = 3
 # block
 B_AIR = "a"
 B_STONE = "b"
@@ -35,5 +45,6 @@ B_STONE = "b"
 COLLISION_OFS = (BLOCK_SZ, BLOCK_SZ)
 
 # **DEBUGGING**
-SHOW_PLAYER_COLLISION = True
+SHOW_PLAYER_COLLISION = False
 PLAYER_FREE_MOVE = False
+SHOW_RAYCAST = True
