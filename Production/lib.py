cda390d6ef8931c6df7fe51464315a475c9267a0
Andrew Shen <39780709+AndrewShen31@users.noreply.github.com>
2018-07-28 08:46:58 +0800

    More accurate editing

--- Production/lib.py
+++ Production/lib.py
@@ -48,28 +48,27 @@ class Character:
         COL = []  # pos of 4 points of character box; rounded to nearest block pos
         x, y, X, Y = self.x, self.y, self.x+CHARACTER_BOX[0]*PIXEL_SZ, self.y+CHARACTER_BOX[1]*PIXEL_SZ
         a, b, A, B = x//BLOCK_SZ*BLOCK_SZ, y//BLOCK_SZ*BLOCK_SZ, X//BLOCK_SZ*BLOCK_SZ, Y//BLOCK_SZ*BLOCK_SZ
-
         for i in range(-BLOCK_SZ*COLLISION_SZ, BLOCK_SZ*COLLISION_SZ+1, BLOCK_SZ):
             for j in range(-BLOCK_SZ*COLLISION_SZ, BLOCK_SZ*COLLISION_SZ+1, BLOCK_SZ):
-                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-TERRAIN_VIEWBOX[0]][y//BLOCK_SZ+j//BLOCK_SZ-TERRAIN_VIEWBOX[1]+1] != BLK["air"]:  # BOTTOM
+                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-COLLISION_OFS[0]][y//BLOCK_SZ+j//BLOCK_SZ-COLLISION_OFS[1]+1] != BLK["air"]:  # BOTTOM
                     if (y < j+B+self.ty < Y+BT_P) and (i+a+self.tx < x < i+A+self.tx or i+a+self.tx < X < i+A+self.tx): COL.append(SOUTH)
-                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-TERRAIN_VIEWBOX[0]][y//BLOCK_SZ+j//BLOCK_SZ-TERRAIN_VIEWBOX[1]] != BLK["air"]:  # TOP
+                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-COLLISION_OFS[0]][y//BLOCK_SZ+j//BLOCK_SZ-COLLISION_OFS[1]-1] != BLK["air"]:  # TOP
                     if (Y > j+b+self.ty > y+TP_P) and (i+a+self.tx < x < i+A+self.tx or i+a+self.tx < X < i+A+self.tx): COL.append(NORTH)
-                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-TERRAIN_VIEWBOX[0]-1][y//BLOCK_SZ+j//BLOCK_SZ-TERRAIN_VIEWBOX[1]] != BLK["air"]:  # LEFT
+                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-COLLISION_OFS[0]-1][y//BLOCK_SZ+j//BLOCK_SZ-COLLISION_OFS[1]] != BLK["air"]:  # LEFT
                     if (X > i+a+self.tx > x+LT_P) and (j+b+self.ty < y < j+B+self.ty or j+b+self.ty < Y < j+B+self.ty): COL.append(WEST)
-                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-TERRAIN_VIEWBOX[0]+1][y//BLOCK_SZ+j//BLOCK_SZ-TERRAIN_VIEWBOX[1]] != BLK["air"]:  # RIGHT
+                if blockt[x//BLOCK_SZ+i//BLOCK_SZ-COLLISION_OFS[0]+1][y//BLOCK_SZ+j//BLOCK_SZ-COLLISION_OFS[1]] != BLK["air"]:  # RIGHT
                     if (x < i+A+self.tx < X+RT_P) and (j+b+self.ty < y < j+B+self.ty or j+b+self.ty < Y < j+B+self.ty): COL.append(EAST)
 
         return COL  # at most two collision, sometime three.
 
     def edit(self, loc, key, chosenBlock):
-        X, Y = self.x+CHARACTER_BOX[0]//2*PIXEL_SZ, self.y+CHARACTER_BOX[1]//2*PIXEL_SZ
-        BXI, BYI = (loc[0]-self.tx)//BLOCK_SZ-1, (loc[1]-self.ty)//BLOCK_SZ-1  # Block's [x/y] index of the block terrain
-        BXL, BYL = (loc[0]-self.tx)//BLOCK_SZ-1*BLOCK_SZ, (loc[1]-self.ty)//BLOCK_SZ-1*BLOCK_SZ  # Block [x/y] location
+        pyg.draw.circle(self.win, (255, 255, 0), (self.x, self.y), EDIT_MAX_CIRCLE*BLOCK_SZ, 4)
+        pyg.draw.rect(self.win, (0, 255, 255), (self.x, self.y, self.x-loc[0], self.y-loc[1]))
+        BXI, BYI = (loc[0]-self.tx)//BLOCK_SZ-TERRAIN_VIEWBOX[0], (loc[1]-self.ty)//BLOCK_SZ-TERRAIN_VIEWBOX[1]  # Block's [x/y] index of the block terrain
         if GK["break"][0] in key: BLOCK = BLK["air"]  # Left  Click - Break - Break into an air
-        elif GK["place"][0] in key: BLOCK = chosenBlock # Right Click - Place - Place onto there chosen block
+        elif GK["place"][0] in key: BLOCK = chosenBlock  # Right Click - Place - Place onto there chosen block
         else: return 0, 0, 0, False
-        return BXI, BYI, BLOCK, True  # Coordinates of BLOCK TERRAIN/Break or Place - POS, BLOCK_ID
+        return BXI, BYI, BLOCK, True
 
     def sneak(self):
         return (CHARACTER_BOX[1], CHARACTER_BOX[0])
